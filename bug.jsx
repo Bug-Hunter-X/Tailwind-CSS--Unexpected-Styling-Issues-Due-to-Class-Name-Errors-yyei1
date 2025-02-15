d6977f215748da7515adaa86579ff5cf19265a96
BugHunter-X <askzenai@gmail.com>
2025-02-15 18:18:05 +0100

    Add bug.jsx file

---
+++ bug.jsx
@@ -0,0 +1,14 @@
+```javascript
+// MyComponent.jsx
+
+function MyComponent() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
+This code uses Tailwind CSS classes, but it does not work correctly if there are any errors in the Tailwind CSS configuration or if a class name is misspelled.
